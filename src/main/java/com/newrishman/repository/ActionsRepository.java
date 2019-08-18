b932d5aee44ff503fe2d240e899dc134e8ceddef
newrishman <safarovrishat@gmail.com>
2019-08-18 16:40:01 +0300

    Spring REST

--- src/main/java/com/newrishman/repository/ActionsRepository.java
+++ src/main/java/com/newrishman/repository/ActionsRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ActionsRepository extends JpaRepository<Actions, Long> {
-    @Query(value = "select * from Actions where job = :job", nativeQuery = true)
+    @Query(value = "select * from Actions where Actions.Job = :job", nativeQuery = true)
     Actions getIdByJob(@Param("job") String job);
 }
