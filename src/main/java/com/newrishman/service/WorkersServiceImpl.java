ebcb4b36096a8fb5b4357091f3fc5cda39a6e53c
newrishman <safarovrishat@gmail.com>
2019-08-21 06:55:29 +0300

    Draft in the noon

--- src/main/java/com/newrishman/service/WorkersServiceImpl.java
+++ src/main/java/com/newrishman/service/WorkersServiceImpl.java
@@ -2,6 +2,7 @@ package com.newrishman.service;
 
 import com.newrishman.domain.Workers;
 import com.newrishman.repository.WorkersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +10,7 @@ public class WorkersServiceImpl implements WorkersService {
 
     private WorkersRepository repository;
 
+    @Autowired
     public void setRepository(WorkersRepository repository) {
         this.repository = repository;
     }
